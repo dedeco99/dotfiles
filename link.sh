0ccf9ebf8c0ae98ef804b9639c13ed5efb35b2fa
dedeco99 <dedeco99@gmail.com>
2020-04-12 20:58:06 +0100

    Added bspwm and sxhkd configs

--- link.sh
+++ link.sh
@@ -2,6 +2,8 @@ cp ./zsh/antigen.zsh ~/antigen.zsh
 cp ./zsh/oh-my-zsh.sh ~/oh-my-zsh.sh
 cp ./zsh/.zshrc ~/.zshrc
 cp ./kitty/kitty.conf ~/.config/kitty/kitty.conf
-cp ./i3/config ~/.i3/config
-cp -r ./i3status/* ~/.config/i3status/
-chmod +x ~/.config/i3status/net-speed.sh
+#cp ./i3/config ~/.i3/config
+#cp -r ./i3status/* ~/.config/i3status/
+#chmod +x ~/.config/i3status/net-speed.sh
+cp ./bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+cp ./sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
