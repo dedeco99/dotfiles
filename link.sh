7a9337939cc924d3414f42a6afdd5da60bc11bde
dedeco99 <dedeco99@gmail.com>
2020-04-13 10:02:51 +0100

    Changed link.sh to handle multiple wm

--- link.sh
+++ link.sh
@@ -1,9 +1,19 @@
-cp ./zsh/antigen.zsh ~/antigen.zsh
-cp ./zsh/oh-my-zsh.sh ~/oh-my-zsh.sh
-cp ./zsh/.zshrc ~/.zshrc
-cp ./kitty/kitty.conf ~/.config/kitty/kitty.conf
-#cp ./i3/config ~/.i3/config
-#cp -r ./i3status/* ~/.config/i3status/
-#chmod +x ~/.config/i3status/net-speed.sh
-cp ./bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-cp ./sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+function parseArgs() {
+	if [ "$1" == "i3" ]; then
+		cp ./i3/config ~/.i3/config
+		cp -r ./i3status/* ~/.config/i3status/
+		chmod +x ~/.config/i3status/net-speed.sh
+	elif [ "$1" == "bspwm" ]; then
+		cp ./bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+		cp ./sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+	fi
+
+	cp ./profile ~/.profile
+	cp ./zsh/antigen.zsh ~/antigen.zsh
+	cp ./zsh/oh-my-zsh.sh ~/oh-my-zsh.sh
+	cp ./zsh/.zshrc ~/.zshrc
+	cp ./kitty/kitty.conf ~/.config/kitty/kitty.conf
+}
+
+
+parseArgs "$@"
