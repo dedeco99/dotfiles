c0a8ce972e0a17cf87aae4b6978e81d80b90f42e
dedeco99 <dedeco99@gmail.com>
2025-10-16 19:35:23 +0100

    added hyprland styling configs

--- link.sh
+++ link.sh
@@ -4,25 +4,33 @@ function parseArgs() {
 	cp ./profile ~/.zprofile
 	cp ./zsh/antigen.zsh ~/antigen.zsh
 	cp ./zsh/zshrc ~/.zshrc
-	[ ! -d ~/.config/kitty ] && mkdir ~/.config/kitty
-	cp ./kitty/kitty.conf ~/.config/kitty/kitty.conf
+	
 	[ ! -d ~/.config/dunst ] && mkdir ~/.config/dunst
 	cp ./dunst/dunstrc ~/.config/dunst/dunstrc
-	[ ! -d ~/.config/gtk-3.0 ] && mkdir ~/.config/gtk-3.0
 	cp ./gtk/gtkrc-2.0 ~/.gtkrc-2.0
-	cp ./gtk/gtkrc-3.0 ~/.config/gtk-3.0/settings.ini
 	[ ! -d ~/.config/mpd ] && mkdir ~/.config/mpd
 	cp ./mpd/mpd.conf ~/.config/mpd/mpd.conf
 	[ ! -d ~/.config/ncmpcpp ] && mkdir ~/.config/ncmpcpp
 	cp ./ncmpcpp/config ~/.config/ncmpcpp/config
 	cp ./ncmpcpp/bindings ~/.config/ncmpcpp/bindings
-	[ ! -d ~/.config/rofi ] && mkdir ~/.config/rofi
-	cp ./rofi/config.rasi ~/.config/rofi/config.rasi
-	cp ./rofi/theme.rasi ~/.config/rofi/theme.rasi
+	
+
 	[ ! -d ~/.config/hypr ] && mkdir ~/.config/hypr
-	cp ./hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+	stow -d ${PWD} -t ~/.config/hypr hypr
 	[ ! -d ~/.config/waybar ] && mkdir ~/.config/waybar
-	cp ./waybar/config ~/.config/waybar/config
+	stow -d ${PWD} -t ~/.config/waybar waybar
+	[ ! -d ~/.config/matugen ] && mkdir ~/.config/matugen
+	stow -d ${PWD} -t ~/.config/matugen matugen
+	[ ! -d ~/.config/kitty ] && mkdir ~/.config/kitty
+	stow -d ${PWD} -t ~/.config/kitty kitty
+	[ ! -d ~/.config/rofi ] && mkdir ~/.config/rofi
+	stow -d ${PWD} -t ~/.config/rofi rofi
+	[ ! -d ~/.config/gtk-2.0 ] && mkdir ~/.config/gtk-2.0
+	stow -d ${PWD} -t ~/.config/gtk-2.0 gtk-2.0
+	[ ! -d ~/.config/gtk-3.0 ] && mkdir ~/.config/gtk-3.0
+	stow -d ${PWD} -t ~/.config/gtk-3.0 gtk-3.0
+	[ ! -d ~/.config/gtk-4.0 ] && mkdir ~/.config/gtk-4.0
+	stow -d ${PWD} -t ~/.config/gtk-4.0 gtk-4.0
 }
 
 parseArgs "$@"
